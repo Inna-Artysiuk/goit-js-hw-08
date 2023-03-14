af8b9dad172192c496f8a919c075612ed36e49b3
Inna-Artysiuk <113395027+Inna-Artysiuk@users.noreply.github.com>
2023-03-14 23:30:10 +0200

    add 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
 // Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// пошук галереї на сторінці
+const gallery = document.querySelector('.gallery');
+
+// створення пустого масиву для додавання елементів
+const items = [];
+
+// перебирання масиву об'єктів з файлу './gallery-items.js'
+galleryItems.forEach(element => {
+  // створення тегу посилання
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = element.original;
+
+  // створення тегу зображення
+  const galleryImage = document.createElement('img');
+
+  // додавання класу до зображення
+  galleryImage.classList.add('gallery__image');
+
+  // додавання малого зображення з властивості об'єкта './gallery-items.js'
+  galleryImage.src = element.preview;
+
+  // додавання атрибуту title з властивості об'єкта './gallery-items.js'
+  galleryImage.setAttribute('title', element.description);
+
+  // додавання тексту для alt з властивості об'єкта './gallery-items.js'
+  galleryImage.alt = element.description;
+
+  galleryLink.append(galleryImage); // вставлення зображення в посилання
+  items.push(galleryLink); // вставлення посилання в масив елементів
+});
+
+// додавання створених елементів до галереї через розпилення
+gallery.append(...items);
+
+// додавання затримки для опису зображення
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
